9eb4f4b1774d2efb2ce187a2119516f5cb08a466
Jim <ray.jim@gmail.com>
2019-07-14 15:40:16 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,28 +7,8 @@ variable "instance_count" {
   default     = 1
 }
 
-variable "ami" {
-  description = "ID of AMI to use for the instance"
-}
-
 variable "instance_type" {
   description = "The type of instance to start"
-}
-
-variable "key_name" {
-  description = "The key name to use for the instance"
-  default     = ""
-}
-
-variable "vpc_security_group_ids" {
-  description = "A list of security group IDs to associate with"
-  type        = "list"
-}
-
-variable "subnet_ids" {
-  description = "A list of VPC Subnet IDs to launch in"
-  default     = []
-  type        = "list"
 }
 
 variable "tags" {
